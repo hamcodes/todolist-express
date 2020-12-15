ad5fe2f3c9015d98b54206efa4ba6d1a1dec78d6
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-12-15 12:22:09 -0500

    added write methods

--- app.js
+++ app.js
@@ -8,12 +8,15 @@ app.get("/", function(req, res){
   var currentDay = today.getDay();
 
   if (currentDay === 6 || currentDay === 0) {
-    res.send("<h1>Yay it's the weekend</h1>");
+    res.write("<h1>Yay it's the weekend</h1>");
   } else {
-    res.send("<h1>Boo, I have to work</h1>");
+    res.write("<p>It is not the weekend.</p>");
+    res.write("<h1>Boo, I have to work</h1>");
+    res.send();
   }
 });
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
 });
+
