7ba911d218cc75a84c919102f5bd5a7e1e7e28c8
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-12-15 14:14:53 -0500

    added css

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
@@ -19,14 +23,15 @@ app.get("/", function(req, res){
 
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", {kindOfDay: day});
+  res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
   var item = req.body.newItem;
-  console.log(item);
-})
+  items.push(item);
+  res.redirect("/");
+});
 
 app.listen(3000, function(){
   console.log("Server started on port 3000");
