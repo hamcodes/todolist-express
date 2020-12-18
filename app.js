8ee3c134031036881049ba4c71dece45e1260bcb
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2020-12-18 11:40:26 -0500

    refactored code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
-let workItems = [];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -14,22 +15,14 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long"
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
 
   res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", function(req, res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   
   if (req.body.list === "Work") {
     workItems.push(item);
@@ -47,9 +40,13 @@ app.get("/work", function(req, res){
 })
 
 app.post("/work", function(req, res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work")
+})
+
+app.get("/about", function(req, res){
+  res.render("about");
 })
 
 app.listen(3000, function(){
